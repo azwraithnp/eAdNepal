aa181368c496419fe16189fc695acbf4cc23c34c
Avinash Mishra <avimshra@gmail.com>
2019-02-20 22:26:19 +0545

    Added field validations to login and register pages

--- app/src/main/java/com/azwraithnp/eadnepal/main/Login/MainActivity.java
+++ app/src/main/java/com/azwraithnp/eadnepal/main/Login/MainActivity.java
@@ -5,19 +5,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.azwraithnp.eadnepal.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button registerButton;
+    Button loginButton, registerButton;
+    EditText email, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        registerButton = findViewById(R.id.btn_reg);
+        setupViews();
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isEmpty(email, password);
+            }
+        });
 
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,5 +34,26 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, RegisterActivity.class));
             }
         });
+
+    }
+
+    public void setupViews()
+    {
+        loginButton = findViewById(R.id.btn_login);
+        registerButton = findViewById(R.id.btn_reg);
+        email = findViewById(R.id.input_email);
+        password = findViewById(R.id.input_password);
+    }
+
+    public void isEmpty(EditText ...edit)
+    {
+        for(EditText editText: edit)
+        {
+            String text = editText.getText().toString();
+            if(text.isEmpty())
+            {
+                editText.setError("Field cannot be empty!");
+            }
+        }
     }
 }
--- app/src/main/java/com/azwraithnp/eadnepal/main/Login/RegisterActivity.java
+++ app/src/main/java/com/azwraithnp/eadnepal/main/Login/RegisterActivity.java
@@ -5,7 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ScrollView;
+import android.widget.Toast;
 
 import com.azwraithnp.eadnepal.R;
 import com.hootsuite.nachos.NachoTextView;
@@ -16,20 +19,32 @@ public class RegisterActivity extends AppCompatActivity {
     FloatingActionButton scrollButton;
     ScrollView scrollView;
     NachoTextView nachoTextView;
+    Button createAccount;
+
+    EditText firstName, lastName, email, phone, age, colgName, companyName, postTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        scrollButton = findViewById(R.id.scrollDownButton);
-        scrollView = findViewById(R.id.scrollView);
-        nachoTextView = findViewById(R.id.nacho_text_view);
+        setupViews();
 
         scrollButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 scrollView.fullScroll(View.FOCUS_DOWN);
+            }
+        });
+
+        createAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isEmpty(firstName, lastName, email, phone, age);
+                if(nachoTextView.getChipValues().size() == 0)
+                {
+                    nachoTextView.setError("Please enter atleast one interest!");
+                }
             }
         });
 
@@ -40,5 +55,33 @@ public class RegisterActivity extends AppCompatActivity {
         nachoTextView.setAdapter(adapter);
 
         nachoTextView.addChipTerminator('\n', ChipTerminatorHandler.BEHAVIOR_CHIPIFY_CURRENT_TOKEN);
+    }
+
+    public void setupViews()
+    {
+        scrollButton = findViewById(R.id.scrollDownButton);
+        scrollView = findViewById(R.id.scrollView);
+        nachoTextView = findViewById(R.id.nacho_text_view);
+        createAccount = findViewById(R.id.btn_signup);
+        firstName = findViewById(R.id.input_fname);
+        lastName = findViewById(R.id.input_lname);
+        email = findViewById(R.id.reg_input_email);
+        phone = findViewById(R.id.input_phone);
+        age = findViewById(R.id.input_age);
+        colgName = findViewById(R.id.input_colg);
+        companyName = findViewById(R.id.input_company);
+        postTitle = findViewById(R.id.input_post);
+    }
+
+    public void isEmpty(EditText ...edit)
+    {
+        for(EditText editText: edit)
+        {
+            String text = editText.getText().toString();
+            if(text.isEmpty())
+            {
+                editText.setError("Field cannot be empty!");
+            }
+        }
     }
 }
